02951f7d19066331f08d914e5803b2f2bbc3239b
Molip-Search <search@molip.io>
2024-12-09 14:02:10 +0900

    csv -> json 컨버팅 작업

--- UnityTools/Assets/Scripts/Util/Editor/Menu.cs
+++ UnityTools/Assets/Scripts/Util/Editor/Menu.cs
@@ -24,5 +24,19 @@ namespace UnityTools.Util
         {
             Directory.Delete(Application.persistentDataPath, true);
         }
+
+        [MenuItem("Util/Convert/CSV to JSON")]
+        static void ConvertCsvToJson()
+        {
+            string csvPath = EditorUtility.OpenFilePanel("Select CSV file", "", "csv");
+            if (string.IsNullOrEmpty(csvPath))
+                return;
+
+            string jsonPath = EditorUtility.SaveFilePanel("Save JSON file", "", "converted.txt", "txt");
+            if (string.IsNullOrEmpty(jsonPath))
+                return;
+
+            CsvJsonConverter.Convert(csvPath, jsonPath);
+        }
     }
 }
